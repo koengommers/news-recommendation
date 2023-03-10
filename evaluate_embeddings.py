0d2bfcedd040a3dd93c1367a07b77ae3399f518c
Koen Gommers <koen@koengommers.nl>
2023-03-10 19:31:20 +0100

    Refactor analogies slightly

--- evaluate_embeddings.py
+++ evaluate_embeddings.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 from sklearn.metrics import top_k_accuracy_score
 from sklearn.metrics.pairwise import cosine_similarity
@@ -7,7 +9,7 @@ def mrr(y_true, y_scores):
     sorted = np.flip(np.argsort(y_scores), axis=-1)
     ranks = []
     for i in range(len(y_true)):
-        rank = np.argwhere(sorted[i] == y_true[i])[0][0]
+        rank = np.argwhere(sorted[i] == y_true[i])[0][0] + 1
         ranks.append(rank)
     ranks = np.array(ranks)
     return np.mean(1 / ranks)
@@ -21,24 +23,29 @@ def evaluate_embeddings(topic_encoder, embeddings):
         encoded = encode(topic)
         return embeddings[encoded]
 
-    comparisons = [
-        (
-            embed("movies-celebrity") - embed("movienews") + embed("musicnews"),
-            "music-celebrity",
-        ),
-        (
-            embed("movies-awards") - embed("movienews") + embed("musicnews"),
-            "music-awards",
-        ),
-        (embed("baseball_mlb") - embed("baseball") + embed("mma"), "mmaufc"),
-        (embed("celebrity"), "celebritynews"),
-        (embed("health-news"), "healthnews"),
+    topic_classes = [
+        # Media and their celebrities
+        [
+            ("movienews", "movies-celebrity"),
+            ("musicnews", "music-celebrity"),
+            ("tvnews", "tv-celebrity"),
+        ],
+        # Sports and their leagues
+        [
+            ("mma", "mmaufc"),
+            ("baseball", "baseball_mlb"),
+        ],
     ]
+    analogies = []
+    for cls in topic_classes:
+        for a, b in product(cls, repeat=2):
+            if a != b:
+                analogies.append((embed(a[1]) - embed(a[0]) + embed(b[0]), b[1]))
 
     y_true = []
     y_scores = []
 
-    for vector, target in comparisons:
+    for vector, target in analogies:
         cosine_similarities = cosine_similarity(vector.reshape(1, -1), embeddings)[0]
         target_index = encode(target)
         y_true.append(target_index)
