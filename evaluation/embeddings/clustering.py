f5f9a9c2cdbd683ced5e683a5ad45c6a28a31663
Koen Gommers <koen@koengommers.nl>
2023-03-16 12:14:36 +0100

    Only keep useful clustering metrics

--- evaluation/embeddings/clustering.py
+++ evaluation/embeddings/clustering.py
@@ -1,8 +1,6 @@
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.metrics import (adjusted_mutual_info_score, adjusted_rand_score,
-                             fowlkes_mallows_score, silhouette_score,
-                             v_measure_score)
+from sklearn.metrics import fowlkes_mallows_score, silhouette_score
 
 
 def get_main_category(categories, subcategory):
@@ -18,29 +16,18 @@ def evaluate_with_clustering(embeddings, dataset, n_times=100):
     ]
     n_clusters = len(set(true_labels))
 
-    rand_scores = []
-    mutual_info_scores = []
     fowlkes_mallows_scores = []
-    v_measure_scores = []
     silhouette_scores = []
 
     for _ in range(n_times):
         kmeans = KMeans(n_clusters=n_clusters, n_init="auto")
         predicted_labels = kmeans.fit_predict(embeddings)
-        rand_scores.append(adjusted_rand_score(true_labels, predicted_labels))
-        mutual_info_scores.append(
-            adjusted_mutual_info_score(true_labels, predicted_labels)
-        )
         fowlkes_mallows_scores.append(
             fowlkes_mallows_score(true_labels, predicted_labels)
         )
-        v_measure_scores.append(v_measure_score(true_labels, predicted_labels))
         silhouette_scores.append(silhouette_score(embeddings, predicted_labels))
 
     return {
-        "Rand index": np.mean(rand_scores),
-        "Mutual information": np.mean(mutual_info_scores),
-        "Fowlkes-Mallows": np.mean(fowlkes_mallows_scores),
-        "V-measure": np.mean(v_measure_scores),
+        "FMI": np.mean(fowlkes_mallows_scores),
         "Silhouette": np.mean(silhouette_scores),
     }
