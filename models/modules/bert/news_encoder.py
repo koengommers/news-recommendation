1417d1ec28e4901c21687f958f30aed6ce8310e8
Koen Gommers <koen@koengommers.nl>
2023-03-27 15:21:13 +0200

    Only finetune last 2 layers

--- models/modules/bert/news_encoder.py
+++ models/modules/bert/news_encoder.py
@@ -6,13 +6,33 @@ from models.modules.attention.additive import AdditiveAttention
 
 
 class NewsEncoder(nn.Module):
-    def __init__(self, bert_config, dropout_probability=0.2, query_vector_dim=200):
+    def __init__(
+        self,
+        bert_config,
+        dropout_probability=0.2,
+        query_vector_dim=200,
+        finetune_n_last_layers=2,
+    ):
         super(NewsEncoder, self).__init__()
         self.dropout_probability = dropout_probability
         self.bert_model = AutoModel.from_config(bert_config)
+
+        # Only finetune last layers
+        if finetune_n_last_layers >= 0:
+            freeze_layers = list(
+                range(bert_config.num_hidden_layers - finetune_n_last_layers)
+            )
+            for name, param in self.bert_model.named_parameters():
+                if name.startswith("embedding"):
+                    param.requires_grad = False
+                if (
+                    name.startswith("encoder")
+                    and int(name.split(".")[2]) in freeze_layers
+                ):
+                    param.requires_grad = False
+
         self.additive_attention = AdditiveAttention(
-            query_vector_dim,
-            bert_config.hidden_size
+            query_vector_dim, bert_config.hidden_size
         )
 
     def forward(self, news):
