b7f4aee9bcfd91f02a210f20c31b0ff12f0c5646
Koen Gommers <koen@koengommers.nl>
2023-04-29 21:07:19 +0200

    Do not load models before instantation

--- src/evaluation/recommender/__init__.py
+++ src/evaluation/recommender/__init__.py
@@ -11,9 +11,6 @@ from tqdm import tqdm
 from datasets.behaviors import BehaviorsDataset, behaviors_collate_fn
 from datasets.news import NewsDataset
 from evaluation.metrics import mrr_score, ndcg_score
-from models.MINER import MINER
-from models.NRMS import NRMS
-from models.TANR import TANR
 from utils.collate import collate_fn
 from utils.encode import CategoricalEncoder
 from utils.tokenize import BertTokenizer, NltkTokenizer
@@ -39,7 +36,7 @@ def calculate_metrics(result):
 
 
 def evaluate(
-    model: Union[NRMS, TANR, MINER],
+    model,
     split: str,
     tokenizer: Union[NltkTokenizer, BertTokenizer],
     categorical_encoders: dict[str, CategoricalEncoder],
