e566664231a08247a983c4870f01d663c5b3dfea
Koen Gommers <koen@koengommers.nl>
2023-05-11 11:08:44 +0200

    chore: cleanup show_config.py

--- src/show_config.py
+++ src/show_config.py
@@ -1,12 +1,12 @@
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
+
+from utils.hydra import print_config
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="train_recommender")
 def main(cfg: DictConfig) -> None:
-    print("========== Config ==========")
-    print(OmegaConf.to_yaml(cfg))
-    print("============================")
+    print_config(cfg)
 
 
 if __name__ == "__main__":
