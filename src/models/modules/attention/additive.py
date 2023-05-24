efd81329bd42058799c8068f7449722699304145
Koen Gommers <koen@koengommers.nl>
2023-05-24 08:43:13 +0200

    feat: use xavier uniform init for attention

--- src/models/modules/attention/additive.py
+++ src/models/modules/attention/additive.py
@@ -14,16 +14,18 @@ class AdditiveAttention(torch.nn.Module):
     def __init__(
         self,
         query_vector_dim: int,
-        candidate_vector_dim: int,
+        embed_dim: int,
     ):
         super(AdditiveAttention, self).__init__()
-        self.linear = nn.Linear(candidate_vector_dim, query_vector_dim)
-        self.attention_query_vector = nn.Parameter(
-            torch.empty(query_vector_dim).uniform_(-0.1, 0.1)
+        self.projection = nn.Linear(embed_dim, query_vector_dim)
+        self.query_vector = nn.Parameter(
+            nn.init.xavier_uniform_(
+                torch.empty(query_vector_dim, 1), gain=nn.init.calculate_gain("tanh")
+            ).squeeze()
         )
 
     def forward(
-        self, candidate_vector: torch.Tensor, mask: Optional[torch.Tensor] = None
+        self, x: torch.Tensor, mask: Optional[torch.Tensor] = None
     ) -> torch.Tensor:
         """
         Args:
@@ -32,15 +34,12 @@ class AdditiveAttention(torch.nn.Module):
             (shape) batch_size, candidate_vector_dim
         """
         # batch_size, candidate_size, query_vector_dim
-        temp = torch.tanh(self.linear(candidate_vector))
+        proj = torch.tanh(self.projection(x))
+        attn_scores = torch.matmul(proj, self.query_vector)
         if mask is not None:
-            temp = temp * mask.unsqueeze(2)
+            attn_scores.masked_fill_(mask == 0, 1e-30)
         # batch_size, candidate_size
-        candidate_weights = F.softmax(
-            torch.matmul(temp, self.attention_query_vector), dim=1
-        )
+        attn_weights = F.softmax(attn_scores, dim=1)
         # batch_size, candidate_vector_dim
-        target = torch.bmm(
-            candidate_weights.unsqueeze(dim=1), candidate_vector
-        ).squeeze(dim=1)
-        return target
+        repr = torch.bmm(attn_weights.unsqueeze(dim=1), x).squeeze(dim=1)
+        return repr
