8bfafc6257243f9d14133a6ea1df0fe110b5f7a8
Koen Gommers <koen@koengommers.nl>
2023-06-09 11:43:58 +0200

    feat: helper function for figsize

---
+++ src/utils/plots.py
@@ -0,0 +1,16 @@
+def get_figsize(fraction=1, aspect_ratio=(5**.5 - 1) / 2, textwidth=483.69687):
+    # Width of figure (in pts)
+    fig_width_pt = textwidth * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * aspect_ratio
+
+    fig_dim = (fig_width_in, fig_height_in)
+
+    return fig_dim
+
